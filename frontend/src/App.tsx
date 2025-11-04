1825db0bf37628f88914b82b55947dab9fa3e1d9
lucas castro <lucas_o__castro@hotmail.com>
2025-11-04 02:02:24 -0300

    Adds authprovider to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,23 @@
 import "./App.css";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
+import "@/plugins/setup";
 import { MantineProvider } from "@mantine/core";
 import { RouterProvider } from "react-router";
 import { router } from "@/routes.tsx";
 import { LocaleProvider } from "@/contexts/LocaleProvider.tsx";
 import { ModalsProvider } from "@mantine/modals";
-import "@/plugins/setup";
+import AuthProvider from "@/contexts/AuthProvider.tsx";
+import { registry } from "@/ioc";
 
 export default function App() {
   return (
     <LocaleProvider>
       <MantineProvider>
         <ModalsProvider>
-          <RouterProvider router={router} />;
+          <AuthProvider authService={registry.get("IAuthService")}>
+            <RouterProvider router={router} />;
+          </AuthProvider>
         </ModalsProvider>
       </MantineProvider>
     </LocaleProvider>
